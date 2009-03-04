5511cf0a06dcbfd92519d554f0f04f00c4e84689
James Darling <james@abscond.org>
2009-03-04 19:37:19 +0000

    moved home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller => 'projects'
+  # map.root :controller => 'projects'
   
-  map.resources :projects
+  map.projects '', :controller => 'projects', :method => 'index'
+  map.resources :projects, :except => [:index]
   
   map.open_id_complete 'session', :controller => "sessions", :action => "create", :conditions => { :method => :get }
   map.resource :session
