962738fd4dfddceae6e3113432607b291b12e570
James Darling <james@abscond.org>
2009-03-09 00:11:37 +0000

    stupid case specific bluecloth

--- config/environment.rb
+++ config/environment.rb
@@ -28,7 +28,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "haml"
-  config.gem "BlueCloth"
+  config.gem "BlueCloth", :lib => "bluecloth"
   
 
   # Only load the plugins named here, in the order given. By default, all plugins 
