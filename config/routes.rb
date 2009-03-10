f04f31e88a178a6a7b95cf1e0ca2321c55405807
James Darling <james@abscond.org>
2009-03-10 12:24:08 +0000

    made multiple pages, and a pretty ugly menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller => 'projects', :action => 'index'
   
   map.resources :projects
   
   map.open_id_complete 'session', :controller => "sessions", :action => "create", :conditions => { :method => :get }
   map.resource :session
+
+  map.pages ':action', :controller => 'pages'
+
 end
