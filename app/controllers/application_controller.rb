eba59a1e9146f5f29006aa351df1b83e7532a73d
James Darling <james@abscond.org>
2009-03-27 18:21:05 +0000

    remove forgery protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery :secret => '108f0192f823f01a6d5169b251ad3b42'
+  # protect_from_forgery :secret => '108f0192f823f01a6d5169b251ad3b42'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
